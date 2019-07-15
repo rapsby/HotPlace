823d436148c359dad22784f606862902357a0839
rapsby <gsu5022@gmail.com>
2019-07-15 23:38:05 +0900

    add parsing of address and phone number

--- venv/Scripts/parsing.py
+++ venv/Scripts/parsing.py
@@ -10,9 +10,10 @@ import pathlib
 import time
 from multiprocessing import Pool
 from urllib.request import urlopen
+from parse import compile
 
 start = time.time()
-
+text = ""
 loc = "1" #input("위치 : ")
 upHpAreaId = 2
 hpAreaId = 1026 #input("AreaId : ")
@@ -24,6 +25,80 @@ kofile = 'C:/Users/k/Desktop/json/ko.xlf'
 kobasefile = 'C:/Users/k/Desktop/json/hhhppp/ko.xlf'
 shutil.copy(basefile, file)
 
+
+
+def get_links():
+    print("link")
+
+    r = requests.get("https://www.siksinhot.com/taste?upHpAreaId=" + str(upHpAreaId) + "&hpAreaId=" + str(hpAreaId) + "&isBestOrd=N")
+    soup = BeautifulSoup(r.text, "html.parser")
+    # 전화번호 parsing을 위한 pid
+    mr = soup.find_all("script")
+    pattern = "\"pid\":[^,]+"
+    r = re.compile(pattern)
+    results = r.findall(str(mr))
+    # 추천 맛집 string 정리
+    it = iter(results)
+    list = []
+    while True:
+        try:
+            pid = next(it)
+            pid = pid[6:]
+            list.append(pid)
+
+        except StopIteration:
+            break
+    return list
+
+def get_content(link):
+    print("content")
+    abs_link = "https://www.siksinhot.com/P/" + link
+    r = requests.get(abs_link)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # 상호명 parsing
+    p = compile("\"pname\":\"{}\"")
+    result = p.parse(soup)
+    pname = result[0]
+
+
+    # 주소 parsing
+    p = compile("\"addr\":\"{}\"")
+    result = p.parse(soup)
+    addr = result[0]
+
+    # 전화번호 parsing
+    p = compile("phone_number\",\"content\":\"{}\"")
+    result = p.parse(soup)
+    phone_number = result[0]
+
+    text += "\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. "+pname+"의 주소는 "+addr+" 입니다. 전화번호는 "+phone_number+"입니다. \n 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
+
+if __name__=='__main__':
+    print("main")
+    start_time = time.time()
+    pool = Pool(processes=8) # 4개의 프로세스를 사용합니다.
+    pool.map(get_content, get_links()) # get_contetn 함수를 넣어줍시다.
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+print(text)
+'''
+            # r = requests.get("https://www.siksinhot.com/P/" + pid)
+            # soup = BeautifulSoup(r.text, "html.parser")
+
+
+            # 주소 parsing
+            mr = soup.find_all("script")
+            pattern = "\"addr\":\"[^\"]+"
+            r = re.compile(pattern)
+            results = r.findall(str(mr))
+            '''
+'''
+if(len(results) == len(list)):
+    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\""
+else:
+    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
+'''
 
 
 """
@@ -66,62 +141,4 @@ os.chdir("C:/Users/k/Desktop/json")
 import zipfile
 with zipfile.ZipFile('namespace.zip', mode='w') as f:
     f.write('ko.xlf', compress_type=zipfile.ZIP_DEFLATED)
-'''
-
-def get_links():
-    print("link")
-
-    r = requests.get("https://www.siksinhot.com/taste?upHpAreaId=" + str(upHpAreaId) + "&hpAreaId=" + str(hpAreaId) + "&isBestOrd=N")
-
-    print(r)
-    soup = BeautifulSoup(r.text, "html.parser")
-    # 전화번호 parsing을 위한 pid
-    mr = soup.find_all("script")
-    pattern = "\"pid\":[^,]+"
-    r = re.compile(pattern)
-    results = r.findall(str(mr))
-    # 추천 맛집 string 정리
-    it = iter(results)
-    list = []
-    while True:
-        try:
-            pid = next(it)
-            pid = pid[6:]
-            list.append(pid)
-
-        except StopIteration:
-            break
-    return list
-
-def get_content(link):
-    print("content")
-    abs_link = "https://www.siksinhot.com/P/" + link
-    r = requests.get(abs_link)
-    html = r.text
-    soup = BeautifulSoup(html, 'html.parser')
-    print(soup.select('h1')[0].text) # 첫 h1 태그를 봅시다.
-
-if __name__=='__main__':
-    print("main")
-    start_time = time.time()
-    pool = Pool(processes=8) # 4개의 프로세스를 사용합니다.
-    pool.map(get_content, get_links()) # get_contetn 함수를 넣어줍시다.
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-'''
-            # r = requests.get("https://www.siksinhot.com/P/" + pid)
-            # soup = BeautifulSoup(r.text, "html.parser")
-
-
-            # 주소 parsing
-            mr = soup.find_all("script")
-            pattern = "\"addr\":\"[^\"]+"
-            r = re.compile(pattern)
-            results = r.findall(str(mr))
-            '''
-'''
-if(len(results) == len(list)):
-    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\""
-else:
-    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
 '''
--- venv/Scripts/test.py
+++ venv/Scripts/test.py
@@ -8,14 +8,10 @@ from collections import OrderedDict
 from pprint import pprint
 import pathlib
 import time
-from multiprocessing import Pool
-from urllib.request import urlopen
-
-
-start_time = time.time()
-loc = "1" #input("위치 : ")
+starttime = time.time()
+loc = input("위치 : ")
 upHpAreaId = 2
-hpAreaId = 1026 #input("AreaId : ")
+hpAreaId = input("AreaId : ")
 
 
 file = 'C:/Users/k/Desktop/json/TempPlace.json'
@@ -24,20 +20,36 @@ kofile = 'C:/Users/k/Desktop/json/ko.xlf'
 kobasefile = 'C:/Users/k/Desktop/json/hhhppp/ko.xlf'
 shutil.copy(basefile, file)
 
-url = 'https://www.siksinhot.com/notice/653'
-print(url)
-r = requests.get(url)
-print(r.text)
-print("--- %s seconds ---" % (time.time() - start_time))
-"""
+r = requests.get("https://www.siksinhot.com/taste?upHpAreaId=" + str(upHpAreaId) + "&hpAreaId="+str(hpAreaId)+"&isBestOrd=N")
+soup = BeautifulSoup(r.text, "html.parser")
+
 # 상호명 parsing
 mr = soup.find_all("script")
 pattern = "\"pname\":\"[^\"]+"
 r = re.compile(pattern)
 results = r.findall(str(mr))
-"""
 
-'''
+# 추천 맛집 string 정리
+it = iter(results)
+list = []
+str = "";
+while True:
+    try:
+
+
+        pname = next(it)
+        pname = pname[9:]
+        list.append(pname)
+        if(len(results) == len(list)):
+            str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\""
+        else:
+            str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
+
+
+
+    except StopIteration:
+        break
+
 # 기본 파일에 위치, 추천 맛집 텍스트 넣기
 import codecs
 fileObj = codecs.open(basefile, "r", "utf-8" )
@@ -69,62 +81,6 @@ os.chdir("C:/Users/k/Desktop/json")
 import zipfile
 with zipfile.ZipFile('namespace.zip', mode='w') as f:
     f.write('ko.xlf', compress_type=zipfile.ZIP_DEFLATED)
-'''
-'''
-def get_links():
-    print("link")
 
-    #r = requests.get("https://www.siksinhot.com/taste?upHpAreaId=" + str(upHpAreaId) + "&hpAreaId=" + str(hpAreaId) + "&isBestOrd=N")
-    r = requests.get('https://www.siksinhot.com/')
-    print(r)
-    soup = BeautifulSoup(r.text, "html.parser")
-    # 전화번호 parsing을 위한 pid
-    mr = soup.find_all("script")
-    pattern = "\"pid\":[^,]+"
-    r = re.compile(pattern)
-    results = r.findall(str(mr))
-    # 추천 맛집 string 정리
-    it = iter(results)
-    list = []
-    while True:
-        try:
-            pid = next(it)
-            pid = pid[6:]
-            list.append(pid)
-
-        except StopIteration:
-            break
-    return list
-
-def get_content(link):
-    print("content")
-    abs_link = "https://www.siksinhot.com/P/" + link
-    r = requests.get(abs_link)
-    html = r.text
-    soup = BeautifulSoup(html, 'html.parser')
-    print(soup.select('h1')[0].text) # 첫 h1 태그를 봅시다.
-
-if __name__=='__main__':
-    print("main")
-    start_time = time.time()
-    pool = Pool(processes=8) # 4개의 프로세스를 사용합니다.
-    pool.map(get_content, get_links()) # get_contetn 함수를 넣어줍시다.
-    print("--- %s seconds ---" % (time.time() - start_time))
-'''
-'''
-            # r = requests.get("https://www.siksinhot.com/P/" + pid)
-            # soup = BeautifulSoup(r.text, "html.parser")
-
-
-            # 주소 parsing
-            mr = soup.find_all("script")
-            pattern = "\"addr\":\"[^\"]+"
-            r = re.compile(pattern)
-            results = r.findall(str(mr))
-            '''
-'''
-if(len(results) == len(list)):
-    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\""
-else:
-    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
-'''
+print("성공")
+print(time.time()-starttime)

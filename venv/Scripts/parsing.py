adca9c8549a6b4bc4aac335ad7dbb42e1d68b7a6
rapsby <gsu2002@naver.com>
2019-07-16 03:50:41 +0900

    complete adding parsed sentence (pname, address, phone number)

--- venv/Scripts/parsing.py
+++ venv/Scripts/parsing.py
@@ -13,6 +13,7 @@ from urllib.request import urlopen
 from parse import compile
 
 start = time.time()
+
 text = ""
 loc = "1" #input("위치 : ")
 upHpAreaId = 2
@@ -32,11 +33,13 @@ def get_links():
 
     r = requests.get("https://www.siksinhot.com/taste?upHpAreaId=" + str(upHpAreaId) + "&hpAreaId=" + str(hpAreaId) + "&isBestOrd=N")
     soup = BeautifulSoup(r.text, "html.parser")
+
     # 전화번호 parsing을 위한 pid
     mr = soup.find_all("script")
     pattern = "\"pid\":[^,]+"
     r = re.compile(pattern)
     results = r.findall(str(mr))
+
     # 추천 맛집 string 정리
     it = iter(results)
     list = []
@@ -51,63 +54,48 @@ def get_links():
     return list
 
 def get_content(link):
-    print("content")
     abs_link = "https://www.siksinhot.com/P/" + link
     r = requests.get(abs_link)
     soup = BeautifulSoup(r.text, "html.parser")
+    string = ""
+    for item in soup.find_all('script'):
+        string = string + str(item.find_all(text=True))
 
     # 상호명 parsing
-    p = compile("\"pname\":\"{}\"")
-    result = p.parse(soup)
-    pname = result[0]
-
-
+    patten = "\"pname\":\"[^\"]+"
+    r = re.compile(patten)
+    results = r.findall(string)
+    pname = results[0]
+    pname = pname[9:]
     # 주소 parsing
-    p = compile("\"addr\":\"{}\"")
-    result = p.parse(soup)
-    addr = result[0]
+    patten = "\"addr\":\"[^\"]+"
+    r = re.compile(patten)
+    results = r.findall(string)
+    addr = results[0]
+    addr = addr[8:]
+
 
     # 전화번호 parsing
-    p = compile("phone_number\",\"content\":\"{}\"")
-    result = p.parse(soup)
-    phone_number = result[0]
+    patten = "phone_number\",\"content\":\"[^\"]+"
+    r = re.compile(patten)
+    results = r.findall(string)
+    phone_number=" X "
+    if len(results) != 0:
+        phone_number = results[0]
+        phone_number = phone_number[25:]
 
-    text += "\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. "+pname+"의 주소는 "+addr+" 입니다. 전화번호는 "+phone_number+"입니다. \n 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
+    #global text
+    #text +=
+    return "\"" + loc + "의 $hotplace-kind 맛집은 " + pname + " 입니다. " + pname + "의 주소는 " + addr + " 입니다. 전화번호는 " + phone_number + " 입니다. \n 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
+
 
 if __name__=='__main__':
     print("main")
     start_time = time.time()
     pool = Pool(processes=8) # 4개의 프로세스를 사용합니다.
-    pool.map(get_content, get_links()) # get_contetn 함수를 넣어줍시다.
+    tmp = pool.map(get_content, get_links()) # get_contetn 함수를 넣어줍시다.
+    print(tmp)
     print("--- %s seconds ---" % (time.time() - start_time))
-
-print(text)
-'''
-            # r = requests.get("https://www.siksinhot.com/P/" + pid)
-            # soup = BeautifulSoup(r.text, "html.parser")
-
-
-            # 주소 parsing
-            mr = soup.find_all("script")
-            pattern = "\"addr\":\"[^\"]+"
-            r = re.compile(pattern)
-            results = r.findall(str(mr))
-            '''
-'''
-if(len(results) == len(list)):
-    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\""
-else:
-    str +="\""+loc+"의 $hotplace-kind 맛집은 "+pname+" 입니다. 다른 음식 추천을 원하시면 다시 음식 종류를 말해주시고 종료를 원하시면 취소를 말씀해주세요.\",\n"
-'''
-
-
-"""
-# 상호명 parsing
-mr = soup.find_all("script")
-pattern = "\"pname\":\"[^\"]+"
-r = re.compile(pattern)
-results = r.findall(str(mr))
-"""
 
 '''
 # 기본 파일에 위치, 추천 맛집 텍스트 넣기
